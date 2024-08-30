34c02f68c84c74f504e6ede3e49d26b671645ac3
Shahidaktar <shahidaktar897@gmail.com>
2024-08-30 11:38:04 +0530

    Changes

--- components/ui/ModelProvider.tsx
+++ components/ui/ModelProvider.tsx
@@ -103,7 +103,7 @@ export const ModalBody = ({
           <motion.div
             ref={modalRef}
             className={cn(
-              "min-h-[50%] max-h-[90%] md:max-h-[60%] md:max-w-[60%] bg-[#111] dark:bg-neutral-950 border border-transparent dark:border-neutral-800 md:rounded-2xl relative z-50 flex flex-col flex-1 overflow-hidden",
+              "min-h-[50%] max-h-[90%] md:max-h-[80%] md:max-w-[60%] bg-[#111] dark:bg-neutral-950 border border-transparent dark:border-neutral-800 md:rounded-2xl relative z-50 flex flex-col flex-1 overflow-hidden",
               className
             )}
             initial={{
